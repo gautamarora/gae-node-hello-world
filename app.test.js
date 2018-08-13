8ca0fe4c6123c639fc95af9788fdb8f8791c372c
Gautam Arora <gautamarora@Gautams-MacBook-Pro.local>
2018-08-13 07:40:44 -0400

    fix tests

--- app.test.js
+++ app.test.js
@@ -8,6 +8,6 @@ describe('App', () => {
     });
     test('should return Hello, World!', async () => {
         const response = await request(app).get('/');
-        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('Hello World!');
     });
 })
