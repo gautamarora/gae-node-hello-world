46675c804484493ceeb90ad473352fafcca2f14d
Gautam Arora <gautamarora@Gautams-MacBook-Pro.local>
2018-08-12 23:09:32 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!!').end();
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
