a77e15da5ec378361382c6e702716cf428fffcde
Gautam Arora <gautamarora@Gautams-MacBook-Pro.local>
2018-08-13 07:28:24 -0400

    adding tests

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!!!').end();
+  res.status(200).send('Hello, world!').end();
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
+module.exports = app
---
+++ app.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('./app')
+
+describe('App', () => {
+    test('should return 200', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+    });
+    test('should return Hello, World!', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+    });
+})
