a68f250deced096c525fe96d597c3b279c8e71fd
Gautam Arora <1215971+gautamarora@users.noreply.github.com>
2018-08-13 15:17:09 -0400

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -6,7 +6,7 @@ describe('App', () => {
         const response = await request(app).get('/');
         expect(response.statusCode).toBe(200);
     });
-    test('should return Hello, World!', async () => {
+    test('should return Hello World!', async () => {
         const response = await request(app).get('/');
         expect(response.text).toBe('Hello World!');
     });
